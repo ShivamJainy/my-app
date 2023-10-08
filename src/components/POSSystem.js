64af3603ba0e850cf227809f98e3e42d63722397
ShivamJainy <shivam.199718@gmail.com>
2023-10-08 22:14:36 +0530

    ADD functionality to computer image

--- src/components/POSSystem.js
+++ src/components/POSSystem.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 import computer from "./../images/computer.jfif"
 
 const POSSystem= () => {
+    const [computerState,setComputer]=useState(0);
+    const ClickComputer=(e)=>{
+        e.preventDefault();
+        setComputer(computerState+1);
+
+    }
+    const cancelBtn=(e)=>{
+        e.preventDefault();
+        window.location.reload();
+    }
     const styles={"wrapper":{"display":"flex","fontSize":"small"},"leftBox":{"width":"40%","backgroundColor":"rgba(238,239,241,255)","marginRight":"0.2%"},"rightBox":{"width":"60%","backgroundColor":"rgba(238,239,241,255)"}};
     return(
         <div style={styles.wrapper}>
@@ -12,9 +22,19 @@ const POSSystem= () => {
                     <div style={{"width":"30%","textAlign":"center"}}>Quantity</div>
                     <div style={{"width":"15%","textAlign":"right"}}>Total</div>
                 </div>
+                {!computerState &&
                 <div style={{"margin":"3%","textAlign":"center","width":"93%","backgroundColor":"white","fontSize":"large","padding":"8% 0"}}>
                     THERE ARE NO PRODUCTS 
                 </div>
+                    }
+                    {computerState &&
+                    <div style={styles.wrapper}>
+                        <div style={{"width":"30%","textAlign":"center"}}>Computer</div>
+                    <div style={{"width":"20%","textAlign":"right"}}>130</div>
+                    <div style={{"width":"30%","textAlign":"center"}}>{computerState}</div>
+                    <div style={{"width":"15%","textAlign":"right"}}>{computerState*130}</div>
+                    </div>
+                    }
                 <div style={{"height":"300px"}}>
                 </div>
                 <div style={{"display":"flex"}}>
@@ -42,13 +62,13 @@ const POSSystem= () => {
                     </table> 
                 </div>
                 <div style={styles.wrapper}>
-                    <button style={{"backgroundColor":"red","width":"50%","color":"white","border":"0","margin":"2%","padding":"3%"}}>Cancle Sale</button>
+                    <button onClick={cancelBtn} style={{"backgroundColor":"red","width":"50%","color":"white","border":"0","margin":"2%","padding":"3%"}}>Cancel Sale</button>
                     <button style={{"backgroundColor":"green","width":"50%","color":"white","border":"0","margin":"2%","padding":"3%"}}>Process Sale</button>
                 </div>
             </div>
             <div style={styles.rightBox}>
                 <figure style={{"display":"contents"}}>
-                <img style={{"height":"100px","width":"100px","marginLeft":"10px","marginTop":"10px"}} src={computer}></img>
+                <img onClick={ClickComputer} style={{"height":"100px","width":"100px","marginLeft":"10px","marginTop":"10px"}} src={computer}></img>
                 <figcaption style={{"textAlign":"center","display":"flex","marginLeft":"30px"}}>Computer</figcaption>
                 </figure>
                 
