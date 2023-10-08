7f33becaa24047f3cdcbb2d9d2b38d273e735319
ShivamJainy <shivam.199718@gmail.com>
2023-10-08 13:11:20 +0530

    Third Commit

--- src/components/POSSystem.js
+++ src/components/POSSystem.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 const POSSystem= () => {
+    const styles={"wrapper":{"display":"flex","fontSize":"small"},"leftBox":{"width":"40%","backgroundColor":"rgba(238,239,241,255)","marginRight":"0.2%"},"rightBox":{"width":"60%","backgroundColor":"rgba(238,239,241,255)"}};
     return(
-        <div>
-            Hi
+        <div style={styles.wrapper}>
+            <div style={styles.leftBox}>
+                <div style={styles.wrapper}>
+                    <div style={{"width":"30%","textAlign":"center"}}>Product</div>
+                    <div style={{"width":"20%","textAlign":"right"}}>Price</div>
+                    <div style={{"width":"30%","textAlign":"center"}}>Quantity</div>
+                    <div style={{"width":"15%","textAlign":"right"}}>Total</div>
+                </div>
+                <div style={{"margin":"3%","textAlign":"center","width":"93%","backgroundColor":"white","fontSize":"large","padding":"8% 0"}}>
+                    THERE ARE NO PRODUCTS 
+                </div>
+                <div>
+                </div>
+            </div>
+            <div style={styles.rightBox}>hi2</div>
         </div>
     )
 };
